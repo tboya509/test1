55a392f7f670c1a3aebbf4cb1062b4e77cf7417b
tboya509 <tboya509@gmail.com>
2024-06-21 02:36:30 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -123,26 +123,26 @@ resource "aws_eks_node_group" "private-nodes2" {
 
 
 
-resource "aws_eks_addon" "example" {
+resource "aws_eks_addon" "vpc-cni" {
   cluster_name = aws_eks_cluster.demo.name
   addon_name   = "vpc-cni"
   addon_version = "v1.11.4-eksbuild.1"
 }
 
-resource "aws_eks_addon" "example" {
+resource "aws_eks_addon" "coredns" {
   cluster_name                = aws_eks_cluster.demo.name
   addon_name                  = "coredns"
   addon_version = "v1.8.7-eksbuild.3"
   resolve_conflicts_on_update = "PRESERVE"
 }
 
-resource "aws_eks_addon" "example" {
+resource "aws_eks_addon" "kube-proxy" {
   cluster_name = aws_eks_cluster.demo.name
   addon_name   = "kube-proxy"
   addon_version = "v1.24.7-eksbuild.2"
 }
 
-resource "aws_eks_addon" "example" {
+resource "aws_eks_addon" "ebs-csi" {
   cluster_name = aws_eks_cluster.demo.name
   addon_name   = "aws-ebs-csi-driver"
   addon_version = "v1.13.0-eksbuild.2"
